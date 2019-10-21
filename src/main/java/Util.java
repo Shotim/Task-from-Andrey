711dc77dc0f9c68273b8ee2ff1de000c6a3acdde
Tsimafei Shostka <tsimafei.shostka@LeverX.local>
2019-10-21 10:19:38 +0300

    Changed the way it works

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -1,5 +1,4 @@
 import java.math.BigDecimal;
-import java.util.Collections;
 import java.util.List;
 
 public class Util {
@@ -13,11 +12,16 @@ public class Util {
 
     public static void method(MovedStock movedStock, List<MaterialStock> basicMaterialStock) {
 
-        BigDecimal resultSum;
-        int elemIndex = Collections.binarySearch(basicMaterialStock, movedStock, (first, second) ->
-                ((MaterialStock) first).getId().compareTo(((MovedStock) second).getId()));
+        for (MaterialStock element : basicMaterialStock) {
+            modifyBasicMaterialStock(movedStock, element);
+        }
 
-        MaterialStock elem = basicMaterialStock.get(elemIndex);
+    }
+
+    private static void modifyBasicMaterialStock(MovedStock movedStock, MaterialStock materialStock) {
+
+        BigDecimal resultSum;
+
         switch (movedStock.getMoveType()) {
             case "201":
             case "202":
@@ -25,21 +29,22 @@ public class Util {
             case "262":
             case "311":
             case "312":
-                resultSum = elem.getUnrestrictedStock().add(movedStock.getMovTypeSumResult());
-                elem.setUnrestrictedStock(resultSum);
+                resultSum = materialStock.getUnrestrictedStock().add(movedStock.getMovTypeSumResult());
+                materialStock.setUnrestrictedStock(resultSum);
                 break;
             case "323":
             case "324":
-                resultSum = elem.getQualityInspectionStock().add(movedStock.getMovTypeSumResult());
-                elem.setQualityInspectionStock(resultSum);
+                resultSum = materialStock.getQualityInspectionStock().add(movedStock.getMovTypeSumResult());
+                materialStock.setQualityInspectionStock(resultSum);
                 break;
             case "325":
             case "326":
-                resultSum = elem.getBlockedStock().add(movedStock.getMovTypeSumResult());
-                elem.setBlockedStock(resultSum);
+                resultSum = materialStock.getBlockedStock().add(movedStock.getMovTypeSumResult());
+                materialStock.setBlockedStock(resultSum);
                 break;
             default:
                 break;
         }
+
     }
 }
